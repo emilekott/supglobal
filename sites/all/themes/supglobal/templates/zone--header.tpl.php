2c31c6c375b2659321bb5d3b244128e6074f575a
Emile Kott <emilekott@gmail.com>
2013-05-14 16:25:45 +0100

    initial theme setup

---
+++ sites/all/themes/supglobal/templates/zone--header.tpl.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * @file
+ * Alpha's theme implementation to display a zone.
+ */
+?>
+<div id="header" class="container-12">
+    <?php print $content; ?>
+</div>
---
+++ sites/all/themes/supglobal/templates/zone--hero.tpl.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * @file
+ * Alpha's theme implementation to display a zone.
+ */
+?>
+<div id="hero">
+    <?php print $content; ?>
+</div>
